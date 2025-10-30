6d08494446360a78df99ec8cafbdf5a040a703f7
Brurur <caesarrianlanang17@gmail.com>
2025-10-31 03:01:11 +0700

    Adjusted Player Movement and Added a Bit of Animation
    Also added TMP.

--- Assets/Player_Animation.cs
+++ Assets/Player_Animation.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player_Animation : MonoBehaviour
 {
     private Rigidbody2D rbPlayer;
+    [SerializeField] Transform playerHands, playerBody, playerWhole;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,8 @@ public class Player_Animation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rbPlayer.linearVelocity.x);
+        playerHands.rotation = Quaternion.Euler(0, 0, rbPlayer.linearVelocity.x * -20);
+        playerBody.rotation = Quaternion.Euler(0, 0, rbPlayer.linearVelocity.x * 4);
+        playerWhole.localScale = new Vector3(Mathf.Clamp(1 - (rbPlayer.linearVelocity.y * 0.1f), 0.75f, 1), Mathf.Clamp(1 - (rbPlayer.linearVelocity.y * -0.05f), 1, 1.5f), 1);
     }
 }
